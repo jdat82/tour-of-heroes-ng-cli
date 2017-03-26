2b6220700d5c6ee02c6997ec87c92145d90df6d7
DAT Jean OAB <jean.dat@orange.com>
2017-03-26 22:21:50 +0200

    feat(hero): Enrich the model to add a more complex hero detail form

--- src/app/core/hero/hero.model.ts
+++ src/app/core/hero/hero.model.ts
@@ -1,5 +1,29 @@
+export enum Sex {
+    Male,
+    Female
+}
+
+export enum HeroCity {
+    GothamCity,
+    KeystoneCity,
+    Metropolis,
+    NewYork,
+    StarCity
+}
+
+export enum Franchise {
+    Marvel,
+    DCComics
+}
 
 export interface Hero {
-  id: number,
-  name: string
+    id:number, // Label
+    name:string, // Input text
+    age?:number, // Slider
+    sex?:Sex, // Button toggle
+    active?:boolean, // Slide toggle
+    city?:HeroCity, // Select
+    powers?: string[] // Chips & autocomplete
+    mainPower?: string // Input with autocomplete
+    franchise?: Franchise // Checkbox list
 }
